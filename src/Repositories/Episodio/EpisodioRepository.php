a92fdde94fbeb5461321dd6fab66cc4dad1b5dd2
andreeezinho <andreeesapucaia@gmail.com>
2025-07-15 22:32:56 -0300

    feat: add method to find episode by number

--- src/Repositories/Episodio/EpisodioRepository.php
+++ src/Repositories/Episodio/EpisodioRepository.php
@@ -166,4 +166,27 @@ class EpisodioRepository implements IEpisodio {
         }
     }
 
+    public function findByNumberAndTempId(int $number, int $temp_id){
+        $stmt = $this->conn->prepare(
+            "SELECT * FROM " . self::TABLE . " 
+                WHERE 
+                    numero = :numero 
+                AND 
+                    temporadas_id = :temp_id
+                "
+        );
+
+        $stmt->bindParam(':id', $id, \PDO::PARAM_INT);
+        $stmt->execute();
+
+        $stmt->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, self::CLASS_NAME);
+        $result = $stmt->fetch();
+
+        if(is_null($result)){
+            return null;
+        }
+
+        return $result;
+    }
+
 }
